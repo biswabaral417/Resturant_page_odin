45393b1a9bab8b35136abb49e639c421791ebe1c
Biswa Baral <131336755+biswabaral417@users.noreply.github.com>
2023-12-05 06:36:11 -0500

    gibi

--- src/modules/component.js
+++ src/modules/component.js
@@ -1,5 +1,4 @@
 const component =(comp)=>{
-    //console.log(comp)
     const content=document.getElementById(`content`)    
     while(content.children[1]){
         content.removeChild(content.children[1])
